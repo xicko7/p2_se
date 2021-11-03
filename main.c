806ce24838be93618c123c51d8486ab79f91c9db
xicko7 <xico.fernandez@udc.es>
2021-11-03 18:05:14 +0100

    Solucionando problemas con init_button()

--- main.c
+++ main.c
@@ -1,39 +1,30 @@
 #include "includes/MKL46Z4.h"
 
-int delay(void)
+int check_button()
 {
-	/*Devolve 1 se se pulsou o botón durante a espera e 0 en caso contrario*/
-	volatile int i;
-
-	for (i = 0; i < 10000; i++){
-		if (check_button())
-			return 1;
-	}
-	return 0;
-}
-
-void led_green_init()
-{
-	SIM_COPC = 0; //Deshabilitar reloxo
-	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK; //Habilitar reloxo no porto 5
-	PORTD_PCR5 = PORT_PCR_MUX(1); //GPIO
-	GPIOD_PDDR |= (1 << 5);
-	GPIOD_PSOR = (1 << 5);
-}
-
-int check_button(){
 	return GPIOC_PDIR;
 }
 
 void init_button()
 {
-	SIM_COPC = 0; //Deshabilitar reloxo
-	PORTC_PCR3 = PORT_PCR_MUX(1); //GPIO
-	GPIOE_PDDR |= (1 << 73);
-	GPIOE_PSOR = (1 << 73);
+	SIM_COPC = 0; // Deshabilitar reloxo
+	PORTC_PCR3 = PORT_PCR_MUX(1);
+	PORTC_PCR3 |= ((uint32_t)(((uint32_t)(1)) << PORT_PCR_PE_SHIFT)) & PORT_PCR_PE_MASK;
+	PORTC_PCR3 |= ((uint32_t)(((uint32_t)(0)) << PORT_PCR_PS_SHIFT)) & PORT_PCR_PS_MASK;
+	GPIOC_PDDR &= (0 << 5); // Input
+	GPIOC_PSOR = (0 << 5);
 }
 
-void led_green_toggle()
+void led_green_init()
+{
+	SIM_COPC = 0;					   // Deshabilitar reloxo
+	SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK; // Habilitar reloxo no porto 5
+	PORTD_PCR5 = PORT_PCR_MUX(1);	   // GPIO
+	GPIOD_PDDR |= (1 << 5);
+	GPIOD_PSOR = (1 << 5);
+}
+
+void led_green_toggle(void)
 {
 	GPIOD_PTOR = (1 << 5);
 }
@@ -52,21 +43,44 @@ void led_red_toggle(void)
 	GPIOE_PTOR = (1 << 29);
 }
 
+int delay(void)
+{
+	/*Devolve 1 se se pulsou o botón durante a espera e 0 en caso contrario*/
+	volatile int i, res = 0;
+
+	for (i = 0; i < 1000000; i++)
+	{
+		// if (check_button() /*& !(i % 1000)*/)
+		// {
+		// 	led_red_toggle();
+		// 	led_green_toggle();
+		// 	res = !res;
+		// }
+	}
+	return res;
+}
+
 int main(void)
 {
 	led_red_init();
 	led_green_init();
+	//init_button();
 
-	int button = 1, red_green = 0;
-	//O primeiro toggle encende o led
-	while (1) {
-		if (red_green)
+	int green_red = 0; // Empeza co verde
+	// O primeiro toggle encende o led
+	while (1)
+	{
+		if (green_red)
+		{
 			led_red_toggle();
+		}
 		else
+		{
 			led_green_toggle();
-		red_green = delay() ? !red_green : red_green;
+		}
+		// Se o botón é pulsado cambia green_red
+		green_red = delay() ? !green_red : green_red;
 	}
 
 	return 0;
 }
-
